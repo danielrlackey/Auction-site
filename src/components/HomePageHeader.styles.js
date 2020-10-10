0e7a88f6a4b188e5e2f86b96338dc21d2edff1b2
Hamza Amer <hamer1563@gmail.com>
2020-10-10 18:23:25 -0400

    Add files via upload

---
+++ src/components/HomePageHeader.styles.js
@@ -0,0 +1,29 @@
+export const styles = theme => ({
+
+
+    background: {
+        backgroundColor: "black",
+        color: "white"
+    },
+
+    title:{
+        fontSize: "70px",
+        textShadow: "2px 2px 4px #000000",
+        fontFamily: 'Anton, sans-serif',
+
+    },
+
+    rankCol:{
+        display: "flex",
+        justifyContent: "center"
+    },
+    hrHead:{
+        margin: "50px 120px 50px 120px"
+    },
+    homePageHeader: {
+        textAlign: "center",
+        marginTop: "150px",
+        marginBottom: "50px"
+    }
+    
+    });
