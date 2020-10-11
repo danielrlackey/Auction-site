50dbb2a72416937da4231414f661421954b78095
Daniel Lackey <danielrlackey0@gmail.com>
2020-10-10 20:54:04 -0400

    something

--- src/components/HomePage.styles.js
+++ src/components/HomePage.styles.js
@@ -9,19 +9,10 @@ export const styles = theme => ({
         backgroundColor: "#fff",
         color: "#c2252f"
     },
-
     rankCol:{
         display: "flex",
         justifyContent: "center"
     },
-    hrHead:{
-        margin: "50px 120px 50px 120px"
-    },
-    homePageHeader: {
-        display: "flex",
-        justifyContent: "center"
-    },
-
     picRow:{
         display: "flex",
         justifyContent: "center"
